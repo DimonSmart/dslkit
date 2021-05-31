8107d21e45b50bdeaacdbd6e2e6a9b74a373700c
DimonSmart <DimonSmart@users.noreply.github.com>
2021-05-31 09:05:16 +0700

    ActionTable prototype

--- DSLKIT/DSLKIT/Parser/TranslationTableBuilder.cs
+++ DSLKIT/DSLKIT/Parser/TranslationTableBuilder.cs
@@ -1,6 +1,8 @@
-﻿using DSLKIT.Base;
+﻿using System;
+using DSLKIT.Base;
 using System.Collections.Generic;
 using System.Linq;
+using DSLKIT.SpecialTerms;
 
 namespace DSLKIT.Parser
 {
@@ -21,4 +23,57 @@ namespace DSLKIT.Parser
             return new TranslationTable(dict);
         }
     }
+
+
+
+    public interface IActionItem
+    {
+    }
+
+    public  class Accept : IActionItem
+    {
+        private Accept()
+        {
+        }
+
+        public static readonly Lazy<Accept> Lazy = new Lazy<Accept>(() => new Accept());
+        public static Accept Instance => Lazy.Value;
+    }
+
+    public class ActionAndGotoTableBuilder
+    {
+        protected readonly Grammar Grammar;
+        protected readonly IEnumerable<RuleSet> RuleSets;
+        public Dictionary<KeyValuePair<RuleSet, ITerm>, IActionItem> ActionTable =
+            new Dictionary<KeyValuePair<RuleSet, ITerm>, IActionItem>();
+        public ActionAndGotoTableBuilder(Grammar grammar, IEnumerable<RuleSet> ruleSets)
+        {
+            Grammar = grammar;
+            RuleSets = ruleSets;
+            Stage1();
+        }
+
+        /// <summary>
+        /// Add a column for the end of input, labeled $.
+        /// Place an "accept" in the $ column whenever the item set contains an item where the pointer is at the end of the starting rule
+        /// (in our example "S → N •").
+        /// </summary>
+        public void Stage1()
+        {
+            foreach (var ruleSet in RuleSets)
+            {
+                if (ContainStartingRuleWithPointerAtTheEnd(ruleSet))
+                {
+                    ActionTable[new KeyValuePair<RuleSet, ITerm>(ruleSet, EofTerminal.Instance)] = Accept.Instance;
+                }
+            }
+        }
+
+        private bool ContainStartingRuleWithPointerAtTheEnd(RuleSet ruleSet)
+        {
+            return ruleSet.Rules.Any(rule => rule.IsFinished && rule.Production.LeftNonTerminal == Grammar.Root);
+        }
+    }
+
+
 }
